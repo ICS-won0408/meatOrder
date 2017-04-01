42d46adacaca9d1fd2ddffee345dd0f9403a15e2
ICS-won0408 <won0408@school.ics.edu.hk>
2017-04-01 20:34:05 +0800

    lots of changes

--- code/write.php
+++ code/write.php
@@ -1,11 +1,27 @@
 <?php
-  if(isset($_POST['pName']) && isset($_POST['pOrder'])){
-    $name = $_POST['pName'];
+  if(isset($_POST['pOrder']) && isset($_POST['pName'])){
     $order = $_POST['pOrder'];
-    $writeToJson = array($name => $order);
-    $fp = fopen('orders.json','w');
-    fwrite($fp, json_encode($writeToJson));
-    fclose($fp);
+    $name = $_POST['pName'];
+    echo($name);
+    $file = 'orders.json';
+
+    $writeToJson = [];
+    $writeToJson[$name] = $order;
+    
+
+
+    $inp = file_get_contents($file);
+    $tempArray = json_decode($inp, true);
+    
+    if(!empty($tempArray)){
+      $tempArray = array_merge($tempArray,$writeToJson);
+      $jsonData = json_encode($tempArray);
+      var_dump($jsonData);
+      file_put_contents($file,$jsonData);
+    }
+    else{
+      file_put_contents($file,json_encode($writeToJson), FILE_APPEND);
+    }
   }
 ?>
 
