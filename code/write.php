b367a7a8de6fe594d68d1e8ab1f277dd10c53d80
ICS-won0408 <won0408@school.ics.edu.hk>
2017-07-17 20:47:33 +0800

    Update write.php

--- code/write.php
+++ code/write.php
@@ -4,7 +4,21 @@
     $name = $_POST['pName'];
     echo($name);
     $file = '../data/orders.json';
+    $meats = '../data/items.json';
+    
 
+    $totalCost = 0.0;
+    $meatCost = json_decode(file_get_contents($meats), true);
+    $keys = array_keys($order);
+
+    var_dump($meatCost);
+    
+    foreach($keys as $key){
+      $numberOfOrders = intval($order[$key]);
+      $totalCost += (floatval(substr($meatCost[$key],1)) * $numberOfOrders);
+    }
+    $order["cost"] = $totalCost;
+    $order["paid"] = 0; 
     $writeToJson = [];
     $writeToJson[$name] = $order;
     
@@ -16,7 +30,6 @@
     if(!empty($tempArray)){
       $tempArray = array_merge($tempArray,$writeToJson);
       $jsonData = json_encode($tempArray);
-      var_dump($jsonData);
       file_put_contents($file,$jsonData);
     }
     else{
@@ -24,5 +37,3 @@
     }
   }
 ?>
-
-    
