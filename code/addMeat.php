9f100d5187c8582cd5a4096c818b3ef42613af13
ICS-won0408 <won0408@school.ics.edu.hk>
2017-08-07 17:16:23 +0800

    bug fixes

--- code/addMeat.php
+++ code/addMeat.php
@@ -5,13 +5,13 @@
  * Date: 8/7/2017
  * Time: 5:02 PM
  */
-if(isset($_POST['meat'], $_POST['meatCost'])) {
+if(isset($_POST['meat'], $_POST['cost'])) {
     $file = '../data/items.json';
     $inp = file_get_contents($file);
     $tempArray = json_decode($inp, true);
 
     $meat = $_POST['meat'];
-    $meatCost = $_POST['meatCost'];
+    $meatCost = $_POST['cost'];
 
     $tempArray[$meat] = '$'.$meatCost;
     file_put_contents($file, json_encode($tempArray));
